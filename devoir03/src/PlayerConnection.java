952c7f6ac6c170bb1e9099b282c7cab9ffe3c234
Rodolpho Pinheiro <rodolphopinheiro@live.com>
2015-04-11 02:57:37 -0400

    cchangement serveur

--- devoir03/src/PlayerConnection.java
+++ devoir03/src/PlayerConnection.java
@@ -1,4 +1,85 @@
+import java.io.*;
+import java.net.*;
 
-public class PlayerConnection {
 
+public class PlayerConnection extends Thread {
+
+	boolean isPartieEnCours;
+	int gridwidth; 
+	int gridheight;
+	
+	private Socket clientSocket;
+	private BufferedReader in;
+	private PrintWriter out;
+	
+	/**
+	 * Constructeur de la classe qui Établissez la socket de connection, et construisez les stream 
+	 * d'entrée et sortie pour communiquer
+	 * @param clientSocket
+	 * @throws ConnectionException
+	 */
+	 public PlayerConnection(Socket clientSocket, int gridwidth, int gridheight) throws ConnectionException {
+		 this.clientSocket = clientSocket;
+		 this.gridwidth=gridwidth;
+		 this.gridheight=gridheight;
+		 
+		 
+		 try {
+			 in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		     out = new PrintWriter(clientSocket.getOutputStream());
+		 }
+		 catch (IOException e) {
+			 System.err.println("Exception en ouvrant les streams du client: " + e);
+		     throw new ConnectionException(e);
+		 }
+	 }
+	 
+	 /*************************************nao sei se tem que usar esse trem nao*********************************/
+	 protected void finalize() {
+		 try {
+			 // cleanup
+		     out.close();
+		     in.close();
+		     clientSocket.close();
+		 }
+		 catch (IOException e) {
+			 System.err.println("IOException en fermant les stream et le socket client" + e);
+		 }
+	 }
+
+	@Override
+	public void run() {
+		
+		try {
+			//Lit le username du joueur evoyé par le client
+			String login = in.readLine();
+			
+			//découvrir le nom de machine du client
+			String nom_de_machine = clientSocket.getInetAddress().getHostName();
+			
+			//
+			
+			
+			
+			
+			
+			/********************************fazer uma execao que presta, pfvr*******************************/	
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	      
+		
+	}
 }
+
+
+
+
+
+
+
+class ConnectionException extends Exception { 
+	  Exception e;
+	  ConnectionException(Exception e) { this.e = e; }
+	  ConnectionException(String msg) { super(msg); e=this; }
+	}
--- devoir03/src/TronServer.java
+++ devoir03/src/TronServer.java
@@ -1,4 +1,26 @@
 
 public class TronServer {
 
+	
+
+	public static void main(String[] args){
+		
+		int serverPort;
+		int clocktick;
+		int gridwidth;
+		int gridheight;
+		
+		//on verifie s'il y a seulement 4 elements en args		
+		if (args.length!=4) {	     
+			System.out.println("Usage: java TronServeur <serverport> <clocktick> <gridwidth> <gridheight>"); 	     
+			System.exit(0);	
+		} else{	
+			serverPort = Integer.parseInt(args[0]);	
+			clocktick = Integer.parseInt(args[1]);		
+			gridwidth = Integer.parseInt(args[2]);		
+			gridheight = Integer.parseInt(args[3]);		
+		}
+		
+		
+	}
 }
