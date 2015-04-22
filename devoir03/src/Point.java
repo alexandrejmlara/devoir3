3dd960fdfe56d0b4b91e6a19a34c291d38eefcac
gabrielhv <gabriel.houlev@hotmail.com>
2015-04-22 15:29:37 -0400

    Create Point.java

---
+++ devoir03/src/Point.java
@@ -0,0 +1,15 @@
+public class Point {
+	
+	public int coordonneeX, coordonneeY;
+	
+	
+	/** Constructeur de Point
+	 * @param x : coordonnée horizontale du point
+	 * @param y : coordonnée verticale du point
+	 */
+	public Point(int x, int y){
+		
+		this.coordonneeX = x;
+		this.coordonneeY = y;
+	}
+}
